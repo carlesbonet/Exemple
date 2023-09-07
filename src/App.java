942b61a24e7eeeea6c0534e026cd8af258bec153
carles bonet <carles.bonet@mataro.epiaedu.cat>
2023-09-07 17:51:42 +0200

    primera versio

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        pelos p = new pelos();
+        p.x=10;
+        System.out.println(p.x);
+
+    }
+}
---
+++ src/pelos.java
@@ -0,0 +1,3 @@
+class pelos {
+    int x;
+}
